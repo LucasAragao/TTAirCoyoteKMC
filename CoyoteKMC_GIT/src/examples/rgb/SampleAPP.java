a18ca2c9f0a801e6bafc619de1be457e37e15da7
lucas <lucas@lucas-Dell-System-XPS-L502X>
2013-01-27 12:26:23 -0300

    Adição do SampleAPP com documentação

---
+++ CoyoteKMC_GIT/src/examples/rgb/SampleAPP.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examples.rgb;
+
+import java.awt.Frame;
+import presentation.ALayerShape;
+import presentation.impl.KinectMotionCapture.KinectControl.KinectAccess;
+import presentation.impl.KinectMotionCapture.layers.LayerRGB;
+import presentation.impl.KinectMotionCapture.layers.ScreenPanel;
+
+/**
+ *
+ * @author lucas
+ */
+public class SampleAPP {
+
+    public static void main(String[] args) {
+        //1º Cria um ScreenPanel
+        ScreenPanel scp = new ScreenPanel();
+        //2º Cria uma variável do tipo ALayerShape e instancia a Layer que deseja exibir. 
+        //Esta por sua vez lança uma exceção que pode se tratada.
+        ALayerShape rgb = null;
+        try {
+            rgb = new LayerRGB();
+
+        } catch (Exception ex) {
+            System.out.println("Não foi possível inicializar a Layer");
+        }
+        //3º Adionamos a Layer no ScreenPanel
+        scp.addLayerShape(rgb);
+        //4º Criamos um Frame
+        Frame f = new Frame();
+        //5º logo após inserimos o ScreenPanel no Frame
+        f.add(scp);
+        //6º Setamos o tamanho do frame, a biblioteca possui um método que retorna
+        //a configuração corrente quanto ao tamanho da tela.
+        f.setSize(KinectAccess.getPrefSize());
+        //7º Setamos o frame como visível
+        f.setVisible(true);
+    }
+}
